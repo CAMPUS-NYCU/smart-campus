faef7c5ddb9f84a3c982950a682a6a034e820cb4
y-c-wang <ycwang.cs@gmail.com>
2023-09-10 11:20:06 +0800

    chore: :technologist: generate ustrusted ssl for local environment
    reference: https://www.npmjs.com/package/@vitejs/plugin-basic-ssl

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite"
+import basicSsl from "@vitejs/plugin-basic-ssl"
 import react from "@vitejs/plugin-react-swc"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [basicSsl(), react()],
   server: {
     port: 4001
   },
