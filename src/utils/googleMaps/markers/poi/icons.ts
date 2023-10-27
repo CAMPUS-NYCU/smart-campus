4e822e4e26da10a8e6deb6404c32fa5c2ea82ff4
hsinlun99 <hsinlun.chiu@gmail.com>
2023-10-27 14:02:08 +0800

    feat: :lipstick: POI icon for each POI status

--- src/utils/googleMaps/markers/poi/icons.ts
+++ src/utils/googleMaps/markers/poi/icons.ts
@@ -1,14 +1,53 @@
-import poiMarkerClean from "../../../../assets/images/poiMarkerClean.svg"; // 清潔狀態
-// import poiMarkerCrowd from "../../../../assets/images/poiMarkerCrowd.svg"; // 人潮狀態
-// import poiMarkerFunction from "../../../../assets/images/poiMarkerFunction.svg"; // 功能狀態
-// import poiMarkerMaintenance from "../../../../assets/images/poiMarkerMaintenance.svg"; // 保養狀態
-// import poiMarkerNoise from "../../../../assets/images/poiMarkerNoise.svg"; // 噪音狀態
-// import poiMarkerOccupation from "../../../../assets/images/poiMarkerOccupation.svg"; // 占用狀態
-// import poiMarkerOutlook from "../../../../assets/images/poiMarkerOutlook.svg"; // 外觀狀態
-// import poiMarkerThermalComfort from "../../../../assets/images/poiMarkerThermalComfort.svg"; // 體感狀態
-// import poiMarkerUsage from "../../../../assets/images/poiMarkerUsage.svg"; // 使用狀態
+import { PoiData } from "../../../../models/poi";
 
-export const getIcon = () => ({
-  url: poiMarkerClean,
-  scaledSize: new google.maps.Size(28, 30),
-});
+import poiMarkerClean from "../../../../assets/images/poiMarkerClean.svg"; // 清潔狀態
+import poiMarkerCrowd from "../../../../assets/images/poiMarkerCrowd.svg"; // 人潮狀態
+import poiMarkerFunction from "../../../../assets/images/poiMarkerFunction.svg"; // 功能狀態
+import poiMarkerMaintenance from "../../../../assets/images/poiMarkerMaintenance.svg"; // 保養狀態
+import poiMarkerNoise from "../../../../assets/images/poiMarkerNoise.svg"; // 噪音狀態
+import poiMarkerOccupation from "../../../../assets/images/poiMarkerOccupation.svg"; // 占用狀態
+import poiMarkerOutlook from "../../../../assets/images/poiMarkerOutlook.svg"; // 外觀狀態
+import poiMarkerThermalComfort from "../../../../assets/images/poiMarkerThermalComfort.svg"; // 體感狀態
+import poiMarkerUsage from "../../../../assets/images/poiMarkerUsage.svg"; // 使用狀態
+import poiMarkerUnknown from "../../../../assets/images/poiMarkerUnknown.svg"; // 未知
+
+export const getIcon = (poiData: PoiData) => {
+  let thisUrl: string;
+  switch (poiData.status) {
+    case "clean":
+      thisUrl = poiMarkerClean;
+      break;
+    case "crowd":
+      thisUrl = poiMarkerCrowd;
+      break;
+    case "function":
+      thisUrl = poiMarkerFunction;
+      break;
+    case "maintenance":
+      thisUrl = poiMarkerMaintenance;
+      break;
+    case "noise":
+      thisUrl = poiMarkerNoise;
+      break;
+    case "occupation":
+      thisUrl = poiMarkerOccupation;
+      break;
+    case "outlook":
+      thisUrl = poiMarkerOutlook;
+      break;
+    case "thermalcomfort":
+      thisUrl = poiMarkerThermalComfort;
+      break;
+    case "usage":
+      thisUrl = poiMarkerUsage;
+      break;
+    default:
+      thisUrl = poiMarkerUnknown;
+      break;
+  }
+
+  return {
+    url: thisUrl,
+    scaledSize: new google.maps.Size(28, 30),
+  };
+};
