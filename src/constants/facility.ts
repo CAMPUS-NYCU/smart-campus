d5ba029e2ab63a14d58cdbcaee44a835e9ecd531
linyc <barney0817.cs10@nycu.edu.tw>
2023-11-06 01:46:25 +0800

    feat(Marker): :sparkles: add facilities data in clusters

--- src/constants/facility.ts
+++ src/constants/facility.ts
@@ -1,6 +1,12 @@
 import { Facilities } from "../models/facility";
 import shineMoodCCData from "../assets/data/markers/facilities/shinemood-cc.json"; // 小木屋&校計中
 import eng3Eng4Data from "../assets/data/markers/facilities/eng3-eng4.json";
+import caCafe1Data from "../assets/data/markers/facilities/ca-cafe1.json";
+import parkingLotCafe2Data from "../assets/data/markers/facilities/parkinglot-cafe2.json";
+import swimMultiGymData from "../assets/data/markers/facilities/swim-multigym.json";
+import outdoorFieldData from "../assets/data/markers/facilities/outdoor-field.json";
+import oldGymData from "../assets/data/markers/facilities/oldgym.json";
+import libraryData from "../assets/data/markers/facilities/library.json";
 
 function getLocations(clusterName: string): Facilities {
   let facilities: Facilities = {};
@@ -13,6 +19,30 @@ function getLocations(clusterName: string): Facilities {
 
     case /^工三&工四/.test(clusterName):
       facilities = eng3Eng4Data;
+      break;
+
+    case /^活動中心&一餐/.test(clusterName):
+      facilities = caCafe1Data;
+      break;
+
+    case /^二餐&停車場/.test(clusterName):
+      facilities = parkingLotCafe2Data;
+      break;
+
+    case /^游泳館&綜合球館/.test(clusterName):
+      facilities = swimMultiGymData;
+      break;
+
+    case /^室外球場/.test(clusterName):
+      facilities = outdoorFieldData;
+      break;
+
+    case /^舊體育館/.test(clusterName):
+      facilities = oldGymData;
+      break;
+
+    case /^圖書館/.test(clusterName):
+      facilities = libraryData;
       break;
 
     default:
