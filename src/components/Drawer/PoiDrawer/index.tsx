b449cd58fbc685c4071f32551f64b5a4f6e3615d
hsinlun <43398440+hsinlun99@users.noreply.github.com>
2023-11-21 21:57:28 +0800

    [Research] poi drawer UI update to meet research 1 requirement (#48)
    * feat: :lipstick: add icons of poiDrawer
    
    * feat: :lipstick: rename statusColor to statusStyle and add icon selection function in it
    
    * feat: :fire: remove unnessary locale items
    
    * feat: :lipstick: update poiDrawer UI to meet research requirement
    
    * fix: :ambulance: fix typos of functional and unoccupied message keys (#47)
    
    * fix: :lipstick: fix images display on poi drawer
    
    * feat: :lipstick: modify button style on poiDrawer to align wih the design
    
    * feat: :fire: remove updatedBy/createdBy information on poiDrawer

--- src/components/Drawer/PoiDrawer/index.tsx
+++ src/components/Drawer/PoiDrawer/index.tsx
@@ -6,6 +6,7 @@ import { Button, Chip, Image } from "@nextui-org/react";
 
 import { useGetPoiQuery } from "../../../api/poi";
 import { useGetUserQuery } from "../../../api/user";
+import { useGetClusterQuery } from "../../../api/cluster";
 import { IRootState } from "../../../store";
 import { openModal } from "../../../store/modal";
 import { editReport } from "../../../store/report";
@@ -16,6 +17,9 @@ import {
 } from "../../../utils/routes/params";
 
 import noImage from "../../../assets/images/noImage.svg";
+import poiDrawerLocation from "../../../assets/images/poiDrawerLocation.svg";
+import poiDrawerTargetSerial from "../../../assets/images/poiDrawerTargetSerial.svg";
+import { statusColor, statusIcon } from "../../../constants/statusStyle";
 import {
   poiStatusType,
   poiStatusTypeMessageKeys,
@@ -30,11 +34,22 @@ const PoiDrawerStatus: React.FC<{ status?: PoiStatusType }> = ({ status }) => {
   const { t } = useTranslation();
 
   return (
-    <Chip>
-      {t(poiStatusTypeMessageKeys[status || poiStatusType.unknown] || "", {
-        ns: ["model"],
-      })}
-    </Chip>
+    <div className="flex flex-row space-x-1 mt-1 items-center">
+      <div className="basis-0.5/12">
+        <Image src={statusIcon(status || poiStatusType.unknown)} alt="status" />
+      </div>
+      <Chip
+        radius="sm"
+        classNames={{
+          content: "px-0.5 text-xs",
+          base: statusColor(status || poiStatusType.unknown),
+        }}
+      >
+        {t(poiStatusTypeMessageKeys[status || poiStatusType.unknown] || "", {
+          ns: ["model"],
+        })}
+      </Chip>
+    </div>
   );
 };
 
@@ -54,6 +69,7 @@ const PoiDrawer: React.FC = () => {
     skip: !selected,
   });
   const { data: user } = useGetUserQuery();
+  const { data: cluster } = useGetClusterQuery(poi?.data.clusterId || "");
 
   const handleDrawerConfirm = () => {
     if (!poi) {
@@ -99,36 +115,85 @@ const PoiDrawer: React.FC = () => {
       open={selected}
       onClose={handleDrawerDismiss}
       title={t("poiDrawer.title", {
-        name: poi?.data.target.name,
+        name: `${poi?.data.target.category}/${poi?.data.target.name}`,
         ns: ["drawer"],
       })}
       children={
-        <div>
-          <div>
-            {t("poiDrawer.content.texts.description", {
-              description: poi?.data.target.serial,
-              ns: ["drawer"],
-            })}
-          </div>
-          <div>
-            {t("poiDrawer.content.texts.latlng", {
-              latitude: poi?.data.latlng.latitude,
-              longitude: poi?.data.latlng.longitude,
-              ns: ["drawer"],
-            })}
-          </div>
-          <PoiDrawerStatus status={poi?.data.status.type} />
-          <div className="flex flex-row">
+        <div className="flex flex-col max-h-[calc(50vh-100px)]">
+          <div className="flex flex-row justify-center basis-6/12 overflow-y-hidden">
             {urls.length > 0 ? (
-              urls.map((url) => <Image key={url} src={url} alt="" />)
+              urls.map((url) => (
+                <Image
+                  key={url}
+                  src={url}
+                  alt="Images of this report"
+                  classNames={{
+                    wrapper: "justify-center flex",
+                    img: "h-[100%]",
+                  }}
+                />
+              ))
             ) : (
-              <Image src={noImage} alt="No image available" />
+              <Image
+                src={noImage}
+                alt="No image available"
+                classNames={{
+                  wrapper: "justify-center flex",
+                  img: "h-[100%]",
+                }}
+              />
             )}
+          </div>
+
+          <div className="basis-6/12">
+            {/* 第一行：location & floor */}
+            <div className="flex flex-row space-x-1 mt-1 items-center">
+              <div className="basis-0.5/12">
+                <Image src={poiDrawerLocation} alt="location and floor" />
+              </div>
+              <Chip radius="sm" classNames={{ content: "px-0.5 text-xs" }}>
+                {t("poiDrawer.content.texts.description", {
+                  description: cluster?.data.name || "",
+                  ns: ["drawer"],
+                })}
+              </Chip>
+              <Chip radius="sm" classNames={{ content: "px-0.5 text-xs" }}>
+                {poi?.data.floor}
+              </Chip>
+            </div>
+
+            {/* 第二行：target serial */}
+            <div className="flex flex-row space-x-1 mt-1 items-center">
+              <div className="basis-0.5/12">
+                <Image
+                  src={poiDrawerTargetSerial}
+                  alt="target serial of this report"
+                />
+              </div>
+              <Chip radius="sm" classNames={{ content: "px-0.5 text-xs" }}>
+                {poi?.data.target.serial}
+              </Chip>
+            </div>
+
+            {/* 第三行：status */}
+            <PoiDrawerStatus status={poi?.data.status.type} />
+
+            {/* 第四行：updatedBy &updatedAt */}
+            <div className="flex flex-row space-x-1 mt-1 items-center justify-end">
+              <p className="text-xs text-secondary">
+                編輯於
+                {poi?.data.updatedAt ? poi.data.updatedAt : poi?.data.createdAt}
+              </p>
+            </div>
           </div>
         </div>
       }
       primaryButton={
-        <Button onClick={handleDrawerConfirm}>
+        <Button
+          radius="full"
+          className="bg-primary h-fit px-2 py-1.5"
+          onClick={handleDrawerConfirm}
+        >
           {t("poiDrawer.buttons.edit", { ns: ["drawer"] })}
         </Button>
       }
---
+++ src/constants/statusStyle.ts
@@ -0,0 +1,89 @@
+import poiDrawerAppearance from "../assets/images/poiDrawerAppearance.svg";
+import poiDrawerCleanliness from "../assets/images/poiDrawerCleanliness.svg";
+import poiDrawerCrowd from "../assets/images/poiDrawerCrowd.svg";
+import poiDrawerFunction from "../assets/images/poiDrawerFunction.svg";
+import poiDrawerMaintenance from "../assets/images/poiDrawerMaintenance.svg";
+import poiDrawerNoise from "../assets/images/poiDrawerNoise.svg";
+import poiDrawerOccupation from "../assets/images/poiDrawerOccupation.svg";
+import poiDrawerThermalComfort from "../assets/images/poiDrawerThermalComfort.svg";
+import poiDrawerUsage from "../assets/images/poiDrawerUsage.svg";
+
+function statusColor(type: string) {
+  let thisColor: string;
+
+  switch (type) {
+    case "maintenance":
+      thisColor = "bg-maintenance";
+      break;
+    case "function":
+      thisColor = "bg-function";
+      break;
+    case "appearance":
+      thisColor = "bg-appearance";
+      break;
+    case "occupation":
+      thisColor = "bg-occupation";
+      break;
+    case "usage":
+      thisColor = "bg-usage";
+      break;
+    case "crowd":
+      thisColor = "bg-crowd";
+      break;
+    case "noise":
+      thisColor = "bg-noise";
+      break;
+    case "thermalComfort":
+      thisColor = "bg-thermalComfort";
+      break;
+    case "cleanliness":
+      thisColor = "bg-cleanliness";
+      break;
+    case "unknown":
+    default:
+      thisColor = "bg-unknown";
+      break;
+  }
+
+  return thisColor;
+}
+
+function statusIcon(type: string) {
+  let thisIcon: string;
+
+  switch (type) {
+    case "maintenance":
+      thisIcon = poiDrawerMaintenance;
+      break;
+    case "function":
+      thisIcon = poiDrawerFunction;
+      break;
+    case "appearance":
+      thisIcon = poiDrawerAppearance;
+      break;
+    case "occupation":
+      thisIcon = poiDrawerOccupation;
+      break;
+    case "usage":
+      thisIcon = poiDrawerUsage;
+      break;
+    case "crowd":
+      thisIcon = poiDrawerCrowd;
+      break;
+    case "noise":
+      thisIcon = poiDrawerNoise;
+      break;
+    case "thermalComfort":
+      thisIcon = poiDrawerThermalComfort;
+      break;
+    case "cleanliness":
+      thisIcon = poiDrawerCleanliness;
+      break;
+    case "unknown":
+    default:
+      thisIcon = "";
+  }
+  return thisIcon;
+}
+
+export { statusColor, statusIcon };
