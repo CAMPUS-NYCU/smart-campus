45240720a18183128a6c854b565303fef2e2bd36
linyc <barney0817.cs10@nycu.edu.tw>
2023-12-02 00:19:52 +0800

    refactor: :zap: use useMemo hook to reduce orderedPoiList rerender times
    orderedPoiList recalculated only when poiList or targetEntry changes

--- src/components/Drawer/ClusterDrawer/index.tsx
+++ src/components/Drawer/ClusterDrawer/index.tsx
@@ -40,7 +40,6 @@ interface PoiListItemProps {
 
 const PoiListItem: React.FC<PoiListItemProps> = (props) => {
   const { poi } = props;
-  console.log("poi input: ", poi);
 
   const containerRef = React.useRef<HTMLDivElement>(null);
 
@@ -230,19 +229,29 @@ const ClusterDrawer: React.FC = () => {
     }
   }, [cluster]);
 
-  let orderedPoiList: Poi[] = [];
+  const orderedPoiList: Poi[] = useMemo(() => {
+    let result: Poi[] = [];
 
-  if (poiList && targetEntry) {
-    orderedPoiList = Object.entries(poiList).map(([id, data]) => ({
-      id,
-      data,
-    }));
-    orderedPoiList.sort((poi1: Poi, poi2: Poi) => {
-      const distance1 = calculateDistance(targetEntry.latlng, poi1.data.latlng);
-      const distance2 = calculateDistance(targetEntry.latlng, poi2.data.latlng);
-      return distance1 - distance2;
-    });
-  }
+    if (poiList && targetEntry) {
+      result = Object.entries(poiList).map(([id, data]) => ({
+        id,
+        data,
+      }));
+      result.sort((poi1: Poi, poi2: Poi) => {
+        const distance1 = calculateDistance(
+          targetEntry.latlng,
+          poi1.data.latlng,
+        );
+        const distance2 = calculateDistance(
+          targetEntry.latlng,
+          poi2.data.latlng,
+        );
+        return distance1 - distance2;
+      });
+    }
+
+    return result;
+  }, [poiList, targetEntry]);
 
   return (
     <Drawer
