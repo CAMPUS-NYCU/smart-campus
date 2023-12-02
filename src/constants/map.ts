9c361344c2704c6f6ee287e36acfe2653c8e67eb
linyc <barney0817.cs10@nycu.edu.tw>
2023-12-03 00:47:30 +0800

    style: :art: change special characters to english name

--- src/constants/map.ts
+++ src/constants/map.ts
@@ -3,14 +3,17 @@ function calculateDistance(
   latlng2: { latitude: number; longitude: number },
 ) {
   const R = 6371e3; // metres
-  const φ1 = (latlng1.latitude * Math.PI) / 180; // φ, λ in radians
-  const φ2 = (latlng2.latitude * Math.PI) / 180;
-  const Δφ = ((latlng2.latitude - latlng1.latitude) * Math.PI) / 180;
-  const Δλ = ((latlng2.longitude - latlng1.longitude) * Math.PI) / 180;
+  const phi1 = (latlng1.latitude * Math.PI) / 180;
+  const phi2 = (latlng2.latitude * Math.PI) / 180;
+  const deltaPhi = ((latlng2.latitude - latlng1.latitude) * Math.PI) / 180;
+  const deltaLambda = ((latlng2.longitude - latlng1.longitude) * Math.PI) / 180;
 
   const a =
-    Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
-    Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+    Math.sin(deltaPhi / 2) * Math.sin(deltaPhi / 2) +
+    Math.cos(phi1) *
+      Math.cos(phi2) *
+      Math.sin(deltaLambda / 2) *
+      Math.sin(deltaLambda / 2);
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
   const d = R * c; // in metres
