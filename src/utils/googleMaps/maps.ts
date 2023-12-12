596716e85d399aa1a10ee62d701c74ee8e9239bb
hsinlun99 <hsinlun.chiu@gmail.com>
2023-12-12 16:11:28 +0800

    feat: :sparkles: set map center for each cluster; set map zoom scale to 18 when click each cluster

--- src/utils/googleMaps/maps.ts
+++ src/utils/googleMaps/maps.ts
@@ -53,3 +53,7 @@ export const getCenter = (): google.maps.LatLng | null => {
 export const getBounds = (): google.maps.LatLngBounds | null => {
   return mapRef.current?.getBounds() || null;
 };
+
+export const setZoom = (zoom: number): void => {
+  mapRef.current?.setZoom(zoom);
+};
