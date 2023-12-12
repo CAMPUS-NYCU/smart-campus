12767848416d8b1e619cec3187c70ae4d7850532
hsinlun99 <hsinlun.chiu@gmail.com>
2023-12-12 17:15:08 +0800

    refactor: :fire: remove chinese comment

--- src/utils/googleMaps/markers/poi/icons.ts
+++ src/utils/googleMaps/markers/poi/icons.ts
@@ -1,25 +1,25 @@
 import { PoiStatusType } from "../../../../models/poi";
 
-import poiMarkerCleanliness from "../../../../assets/images/poiMarkerCleanliness.svg"; // 清潔狀態
-import poiMarkerCrowd from "../../../../assets/images/poiMarkerCrowd.svg"; // 人潮狀態
-import poiMarkerFunction from "../../../../assets/images/poiMarkerFunction.svg"; // 功能狀態
-import poiMarkerMaintenance from "../../../../assets/images/poiMarkerMaintenance.svg"; // 保養狀態
-import poiMarkerNoise from "../../../../assets/images/poiMarkerNoise.svg"; // 噪音狀態
-import poiMarkerOccupation from "../../../../assets/images/poiMarkerOccupation.svg"; // 占用狀態
-import poiMarkerAppearance from "../../../../assets/images/poiMarkerAppearance.svg"; // 外觀狀態
-import poiMarkerThermalComfort from "../../../../assets/images/poiMarkerThermalComfort.svg"; // 體感狀態
-import poiMarkerUsage from "../../../../assets/images/poiMarkerUsage.svg"; // 使用狀態
-import poiMarkerUnknown from "../../../../assets/images/poiMarkerUnknown.svg"; // 未知
+import poiMarkerCleanliness from "../../../../assets/images/poiMarkerCleanliness.svg";
+import poiMarkerCrowd from "../../../../assets/images/poiMarkerCrowd.svg";
+import poiMarkerFunction from "../../../../assets/images/poiMarkerFunction.svg";
+import poiMarkerMaintenance from "../../../../assets/images/poiMarkerMaintenance.svg";
+import poiMarkerNoise from "../../../../assets/images/poiMarkerNoise.svg";
+import poiMarkerOccupation from "../../../../assets/images/poiMarkerOccupation.svg";
+import poiMarkerAppearance from "../../../../assets/images/poiMarkerAppearance.svg";
+import poiMarkerThermalComfort from "../../../../assets/images/poiMarkerThermalComfort.svg";
+import poiMarkerUsage from "../../../../assets/images/poiMarkerUsage.svg";
+import poiMarkerUnknown from "../../../../assets/images/poiMarkerUnknown.svg";
 
-import poiMarkerHighlightedAppearance from "../../../../assets/images/poiMarkerHighlightedAppearance.svg"; // 高亮外觀狀態
-import poiMarkerHighlightedCleanliness from "../../../../assets/images/poiMarkerHighlightedCleanliness.svg"; // 高亮清潔狀態
-import poiMarkerHighlightedCrowd from "../../../../assets/images/poiMarkerHighlightedCrowd.svg"; // 高亮人潮狀態
-import poiMarkerHighlightedFunction from "../../../../assets/images/poiMarkerHighlightedFunction.svg"; // 高亮功能狀態
-import poiMarkerHighlightedMaintenance from "../../../../assets/images/poiMarkerHighlightedMaintenance.svg"; // 高亮保養狀態
-import poiMarkerHighlightedNoise from "../../../../assets/images/poiMarkerHighlightedNoise.svg"; // 高亮噪音狀態
-import poiMarkerHighlightedOccupation from "../../../../assets/images/poiMarkerHighlightedOccupation.svg"; // 高亮占用狀態
-import poiMarkerHighlightedThermalComfort from "../../../../assets/images/poiMarkerHighlightedThermalComfort.svg"; // 高亮體感狀態
-import poiMarkerHighlightedUsage from "../../../../assets/images/poiMarkerHighlightedUsage.svg"; // 高亮使用狀態
+import poiMarkerHighlightedAppearance from "../../../../assets/images/poiMarkerHighlightedAppearance.svg";
+import poiMarkerHighlightedCleanliness from "../../../../assets/images/poiMarkerHighlightedCleanliness.svg";
+import poiMarkerHighlightedCrowd from "../../../../assets/images/poiMarkerHighlightedCrowd.svg";
+import poiMarkerHighlightedFunction from "../../../../assets/images/poiMarkerHighlightedFunction.svg";
+import poiMarkerHighlightedMaintenance from "../../../../assets/images/poiMarkerHighlightedMaintenance.svg";
+import poiMarkerHighlightedNoise from "../../../../assets/images/poiMarkerHighlightedNoise.svg";
+import poiMarkerHighlightedOccupation from "../../../../assets/images/poiMarkerHighlightedOccupation.svg";
+import poiMarkerHighlightedThermalComfort from "../../../../assets/images/poiMarkerHighlightedThermalComfort.svg";
+import poiMarkerHighlightedUsage from "../../../../assets/images/poiMarkerHighlightedUsage.svg";
 
 export const getIcon = (status: PoiStatusType): google.maps.Icon => {
   let thisUrl: string;
