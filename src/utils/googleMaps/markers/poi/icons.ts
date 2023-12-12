b14bf5728c0f6bbc4833e09e1263476552cf8088
hsinlun99 <hsinlun.chiu@gmail.com>
2023-12-12 16:51:43 +0800

    feat: :memo: add highlighted poi icons

--- src/utils/googleMaps/markers/poi/icons.ts
+++ src/utils/googleMaps/markers/poi/icons.ts
@@ -11,6 +11,16 @@ import poiMarkerThermalComfort from "../../../../assets/images/poiMarkerThermalC
 import poiMarkerUsage from "../../../../assets/images/poiMarkerUsage.svg"; // 使用狀態
 import poiMarkerUnknown from "../../../../assets/images/poiMarkerUnknown.svg"; // 未知
 
+import poiMarkerHighlightedAppearance from "../../../../assets/images/poiMarkerHighlightedAppearance.svg"; // 高亮外觀狀態
+import poiMarkerHighlightedCleanliness from "../../../../assets/images/poiMarkerHighlightedCleanliness.svg"; // 高亮清潔狀態
+import poiMarkerHighlightedCrowd from "../../../../assets/images/poiMarkerHighlightedCrowd.svg"; // 高亮人潮狀態
+import poiMarkerHighlightedFunction from "../../../../assets/images/poiMarkerHighlightedFunction.svg"; // 高亮功能狀態
+import poiMarkerHighlightedMaintenance from "../../../../assets/images/poiMarkerHighlightedMaintenance.svg"; // 高亮保養狀態
+import poiMarkerHighlightedNoise from "../../../../assets/images/poiMarkerHighlightedNoise.svg"; // 高亮噪音狀態
+import poiMarkerHighlightedOccupation from "../../../../assets/images/poiMarkerHighlightedOccupation.svg"; // 高亮占用狀態
+import poiMarkerHighlightedThermalComfort from "../../../../assets/images/poiMarkerHighlightedThermalComfort.svg"; // 高亮體感狀態
+import poiMarkerHighlightedUsage from "../../../../assets/images/poiMarkerHighlightedUsage.svg"; // 高亮使用狀態
+
 export const getIcon = (status: PoiStatusType): google.maps.Icon => {
   let thisUrl: string;
   switch (status) {
@@ -49,5 +59,47 @@ export const getIcon = (status: PoiStatusType): google.maps.Icon => {
   return {
     url: thisUrl,
     scaledSize: new google.maps.Size(28, 30),
+  };
+};
+
+export const getHighlightedIcon = (status: PoiStatusType): google.maps.Icon => {
+  let thisUrl: string;
+
+  switch (status) {
+    case "cleanliness":
+      thisUrl = poiMarkerHighlightedCleanliness;
+      break;
+    case "crowd":
+      thisUrl = poiMarkerHighlightedCrowd;
+      break;
+    case "function":
+      thisUrl = poiMarkerHighlightedFunction;
+      break;
+    case "maintenance":
+      thisUrl = poiMarkerHighlightedMaintenance;
+      break;
+    case "noise":
+      thisUrl = poiMarkerHighlightedNoise;
+      break;
+    case "occupation":
+      thisUrl = poiMarkerHighlightedOccupation;
+      break;
+    case "appearance":
+      thisUrl = poiMarkerHighlightedAppearance;
+      break;
+    case "thermalComfort":
+      thisUrl = poiMarkerHighlightedThermalComfort;
+      break;
+    case "usage":
+      thisUrl = poiMarkerHighlightedUsage;
+      break;
+    default:
+      thisUrl = poiMarkerUnknown;
+      break;
+  }
+
+  return {
+    url: thisUrl,
+    scaledSize: new google.maps.Size(45, 45),
   };
 };
