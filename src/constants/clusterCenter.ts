f4445e9ead0c83c6a618401a79bbb95f0d4c9451
hsinlun99 <hsinlun.chiu@gmail.com>
2024-03-14 15:13:48 +0800

    fix: :ambulance: fix regular expression for the new cluserName

--- src/constants/clusterCenter.ts
+++ src/constants/clusterCenter.ts
@@ -2,8 +2,12 @@ import centerData from "../assets/data/clusterCenters/clusterCenter.json";
 
 function getClusterCenter(clusterName: string) {
   if (clusterName) {
-    const matches = clusterName.match(/[^a-zA-Z\d]+/g);
-    const pureClusterName = matches ? matches.join("") : "";
+    // for research requirement. other cluster name may not be properly handled
+    // 1. remove the number after a Chinese character of the cluster name
+    // 2. remove the character from the dash (-) to the end of the cluster name
+    const pureClusterName = clusterName
+      .replace(/(\p{Script=Han})\d+/gu, "$1")
+      .replace(/-.*$/, "");
 
     const foundCenter = centerData.find(
       (center) => center.name === pureClusterName,
@@ -11,6 +15,9 @@ function getClusterCenter(clusterName: string) {
 
     if (foundCenter) {
       return foundCenter;
+    } else {
+      console.warn(`Cluster center not found: ${clusterName}`);
+      return null;
     }
   }
 
