5e1c5730fa9bd8d08db48d614de752be23eb83a8
hsinlun99 <hsinlun.chiu@gmail.com>
2024-03-14 14:49:49 +0800

    refactor: :bulb: update the comment for drawerTitle

--- src/constants/drawerTitle.ts
+++ src/constants/drawerTitle.ts
@@ -1,7 +1,7 @@
 function getDrawerTitle(clusterName: string) {
   // for research requirement. other cluster name may not be properly handled
   // 1. remove the number after a Chinese character of the cluster name
-  // 2. remove the dash (-) to the end of the cluster name
+  // 2. remove the character from the dash (-) to the end of the cluster name
   return clusterName.replace(/(\p{Script=Han})\d+/gu, "$1").replace(/-.*$/, "");
 }
 
