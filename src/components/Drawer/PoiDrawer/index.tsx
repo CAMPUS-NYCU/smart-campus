1fce74c1dd3ecb774cdce1d8fa8c0a359d939d1c
hsinlun99 <hsinlun.chiu@gmail.com>
2024-03-21 14:21:01 +0800

    refactor: :lipstick: remove the update button on poiDrawer

--- src/components/Drawer/PoiDrawer/index.tsx
+++ src/components/Drawer/PoiDrawer/index.tsx
@@ -1,15 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { useSearchParams } from "react-router-dom";
-import { Button, Chip, Image } from "@nextui-org/react";
+import { Chip, Image } from "@nextui-org/react";
 
 import { useGetPoiQuery } from "../../../api/poi";
-import { useGetUserQuery } from "../../../api/user";
 import { useGetClusterQuery } from "../../../api/cluster";
 import { IRootState } from "../../../store";
-import { openModal } from "../../../store/modal";
-import { editReport } from "../../../store/report";
 import {
   getParamsFromDrawer,
   isCurrentDrawerParams,
@@ -64,26 +61,13 @@ const PoiDrawer: React.FC = () => {
 
   const reportType = useSelector((state: IRootState) => state.report.type);
 
-  const dispatch = useDispatch();
-
   const selected = !reportType && isCurrentDrawerParams("poi", searchParams);
   const id = selected ? getParamsFromDrawer("poi", searchParams).poiId : null;
 
   const { data: poi } = useGetPoiQuery(id, {
     skip: !selected,
   });
-  const { data: user } = useGetUserQuery();
   const { data: cluster } = useGetClusterQuery(poi?.data.clusterId || "");
-
-  const handleDrawerConfirm = () => {
-    if (!poi) {
-      throw new Error("ClusterDrawer: poi not found");
-    } else if (!user?.id) {
-      dispatch(openModal("login"));
-    } else {
-      dispatch(editReport(poi));
-    }
-  };
 
   const handleDrawerDismiss = () => {
     if (!poi) {
@@ -123,7 +107,7 @@ const PoiDrawer: React.FC = () => {
         ns: ["drawer"],
       })}
       children={
-        <div className="flex flex-col max-h-[calc(50vh-100px)]">
+        <div className="flex flex-col max-h-[calc(50vh-32px)]">
           <div className="flex flex-row justify-center basis-6/12 overflow-y-hidden">
             {urls.length > 0 ? (
               urls.map((url) => (
@@ -198,15 +182,6 @@ const PoiDrawer: React.FC = () => {
             </div>
           </div>
         </div>
-      }
-      primaryButton={
-        <Button
-          radius="full"
-          className="bg-primary h-fit px-2 py-1.5"
-          onClick={handleDrawerConfirm}
-        >
-          {t("poiDrawer.buttons.edit", { ns: ["drawer"] })}
-        </Button>
       }
     />
   );
