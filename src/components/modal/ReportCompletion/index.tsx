8f424f13561232a41c6c44b6de10b1984b2c9b88
hsinlun99 <hsinlun.chiu@gmail.com>
2024-03-22 00:49:18 +0800

    feat: :sparkles: made a modal for report completion prompt

---
+++ src/components/modal/ReportCompletion/index.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+} from "@nextui-org/react";
+
+import { IRootState } from "../../../store";
+import { toggleModal } from "../../../store/modal";
+
+const ReportCompletion: React.FC = () => {
+  const { t } = useTranslation();
+
+  const modalOpen = useSelector(
+    (state: IRootState) => state.modal.open["reportCompletion"],
+  );
+
+  const dispatch = useDispatch();
+
+  const handleToggleModal = () => {
+    dispatch(toggleModal("reportCompletion"));
+  };
+
+  return (
+    <Modal
+      isOpen={modalOpen}
+      onOpenChange={handleToggleModal}
+      classNames={{
+        base: "h-1/5",
+      }}
+    >
+      <ModalContent>
+        <ModalHeader />
+        <ModalBody className="items-center justify-center">
+          <p className="text-xl font-bold">
+            {t("reportCompletion.text", { ns: ["modal"] })}
+          </p>
+        </ModalBody>
+        <ModalFooter />
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default ReportCompletion;
