75312801c0e07d19d60c21d6283333d09d1195ce
Barney Lin <32840701+linyc0817@users.noreply.github.com>
2024-04-25 15:22:16 +0800

    [Basic] Modify router query string (#129)
    * feat: :sparkles: Modify router query string
    
    * fix: :bug: fix toggle highlightedPoi issue: sometimes poi marker cannot be highlighted properly
    
    * fix: :bug: fix `reportStart` modal pop up timing issue: it pop up when closing poi drawer while it isn't an expected  behavior
    
    * docs: :memo: update cluster center for no-list version
    
    ---------
    
    Co-authored-by: hsinlun99 <hsinlun.chiu@gmail.com>

--- src/components/Map/Markers/PoiMarkers/index.tsx
+++ src/components/Map/Markers/PoiMarkers/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useSearchParams } from "react-router-dom";
 
-import { useGetPoiQuery, useGetPoisQuery } from "../../../../api/poi";
+import { useGetPoisQuery } from "../../../../api/poi";
 import { IRootState } from "../../../../store";
 import { setHighlightId } from "../../../../store/poi";
 import { markers } from "../../../../utils/googleMaps";
@@ -14,37 +14,34 @@ const PoiMarkers: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
 
   const highlightId = useSelector((state: IRootState) => state.poi.highlightId);
+
   const prevHighlightId = React.useRef<string | null>("");
 
   const clusterId = getParamsFromDrawer("cluster", searchParams).clusterId;
   const { data: pois } = useGetPoisQuery(clusterId);
 
-  const poiId = getParamsFromDrawer("poi", searchParams).poiId;
-  const { data: poi } = useGetPoiQuery(poiId);
-
   const dispatch = useDispatch();
 
   const handleClick = React.useCallback(
     (poiId: string) => {
-      setSearchParams({ poiId });
-      dispatch(setHighlightId(poiId));
+      if (poiId !== highlightId) {
+        setSearchParams({ clusterId, poiId });
+        dispatch(setHighlightId(poiId));
+      }
     },
-    [dispatch, setSearchParams],
+    [dispatch, setSearchParams, clusterId, highlightId],
   );
 
   React.useEffect(() => {
     if (pois && Object.keys(pois).length !== 0) {
       markers.poi.setPois(pois);
       setOnPoiMarkerClick(handleClick);
-    } else if (poi) {
-      markers.poi.setPois({ [poi.id]: poi.data });
-      setOnPoiMarkerClick(handleClick);
     }
 
     return () => {
       markers.poi.clear();
     };
-  }, [poi, pois, handleClick]);
+  }, [pois, handleClick]);
 
   React.useEffect(() => {
     if (highlightId && pois && pois[highlightId]) {
@@ -72,19 +69,6 @@ const PoiMarkers: React.FC = () => {
 
     prevHighlightId.current = highlightId;
   }, [highlightId, pois]);
-
-  // in order to show poi drawer, we setParameter to poiId, only have poi now
-  React.useEffect(() => {
-    if (poi) {
-      const { latitude, longitude } = poi.data.latlng;
-      const north = maps.getBounds()?.getNorthEast().lat();
-      const south = maps.getBounds()?.getSouthWest().lat();
-      maps.panTo(
-        north && south ? latitude - (north - south) / 4 : latitude,
-        longitude,
-      );
-    }
-  }, [poi]);
   return <></>;
 };
 
