d7394692274200b4de4088f0897545b9f313e4b2
linyc <barney0817.cs10@nycu.edu.tw>
2024-04-26 17:42:23 +0800

    feat(Marker): :sparkles: Add facility filter button and drawer with options

--- src/components/Map/Fabs/FacilityFilterFab/index.tsx
+++ src/components/Map/Fabs/FacilityFilterFab/index.tsx
@@ -8,6 +8,9 @@ import { useDispatch, useSelector } from "react-redux";
 
 import { setSelectedCategories } from "../../../../store/facility";
 import { IRootState } from "../../../../store";
+import facilityMarkerFilter from "../../../../assets/images/facilityMarkerFilter.svg";
+import { openModal } from "../../../../store/modal";
+import FacilityFilter from "../../../modal/FacilityFilter";
 
 const FacilityFilterFabs: React.FC = () => {
   const [searchParams] = useSearchParams();
@@ -44,32 +47,51 @@ const FacilityFilterFabs: React.FC = () => {
     }
   };
 
-  return (
-    <div className="absolute inset-0 w-3/4 h-9 top-8 left-4 overflow-x-auto scrollbar-hide">
-      <div className="flex gap-2 items-center justify-left">
-        {categories.map((category, index) => (
-          <Button
-            key={category + index}
-            className={`shrink-0 \
-                    bg-zinc-50 dark:bg-zinc-800 \
-                      text-black dark:text-white \
-                      border border-zinc-400 dark:border-zinc-900 \
-                      shadow-sm shadow-zinc-300 dark:shadow-zinc-800 
-                      ${
-                        selectedCategories.includes(category)
-                          ? "bg-textBtnHover dark:bg-textBtnHover text-white dark:text-white"
-                          : ""
-                      }`}
-            color="primary"
-            size="sm"
-            onClick={() => handleCategoryClick(category)}
-          >
-            {category}
-          </Button>
-        ))}
+  const handleFilterClick = () => {
+    dispatch(openModal("facilityFilter"));
+  };
+
+  return clusterId ? (
+    <>
+      <div className="absolute inset-0 w-3/4 h-9 top-8 left-4 overflow-x-auto scrollbar-hide">
+        <div className="flex gap-2 items-center justify-left">
+          {categories.map((category, index) => (
+            <Button
+              key={category + index}
+              className={`shrink-0 \
+                      bg-zinc-50 dark:bg-zinc-800 \
+                        text-black dark:text-white \
+                        border border-zinc-400 dark:border-zinc-900 \
+                        shadow-sm shadow-zinc-300 dark:shadow-zinc-800 
+                        ${
+                          selectedCategories.includes(category)
+                            ? "bg-textBtnHover dark:bg-textBtnHover text-white dark:text-white"
+                            : ""
+                        }`}
+              color="primary"
+              size="sm"
+              onClick={() => handleCategoryClick(category)}
+            >
+              {category}
+            </Button>
+          ))}
+        </div>
       </div>
-    </div>
-  );
+      <Button
+        key="FacilityFilterFab"
+        className="absolute top-20 right-4 w-10 h-10"
+        isIconOnly
+        size="sm"
+        style={{ backgroundColor: "transparent", padding: 0 }}
+        onClick={() => {
+          handleFilterClick();
+        }}
+      >
+        <img src={facilityMarkerFilter} alt="facilityMarkerFilter" />
+      </Button>
+      <FacilityFilter />
+    </>
+  ) : null;
 };
 
 export default FacilityFilterFabs;
---
+++ src/components/modal/FacilityFilter/index.tsx
@@ -0,0 +1,88 @@
+import React from "react";
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalHeader,
+} from "@nextui-org/react";
+import { useDispatch, useSelector } from "react-redux";
+import { IRootState } from "../../../store";
+import { toggleModal } from "../../../store/modal";
+import { useSearchParams } from "react-router-dom";
+import { getParamsFromDrawer } from "../../../utils/routes/params";
+import { useGetClusterQuery } from "../../../api/cluster";
+import { getLocationCategories } from "../../../constants/facility";
+import { setSelectedCategories } from "../../../store/facility";
+
+const FacilityFilter: React.FC = () => {
+  const [searchParams] = useSearchParams();
+
+  const clusterId = getParamsFromDrawer("cluster", searchParams).clusterId;
+  const { data: cluster } = useGetClusterQuery(clusterId);
+
+  const categories = cluster ? getLocationCategories(cluster.data.name) : [];
+  const selectedCategories = useSelector(
+    (state: IRootState) => state.facility.selectedCategories,
+  );
+
+  const modalOpen = useSelector(
+    (state: IRootState) => state.modal.open["facilityFilter"],
+  );
+
+  const dispatch = useDispatch();
+
+  const handleToggleModal = () => {
+    dispatch(toggleModal("facilityFilter"));
+  };
+
+  const handleCategoryClick = (category: string) => {
+    if (selectedCategories.includes(category)) {
+      // If the category is already selected, remove it
+      dispatch(
+        setSelectedCategories(
+          selectedCategories.filter((cat) => cat !== category),
+        ),
+      );
+    } else {
+      // If the category is not selected, add it
+      dispatch(setSelectedCategories([...selectedCategories, category]));
+    }
+  };
+
+  return (
+    <Modal isOpen={modalOpen} onOpenChange={handleToggleModal}>
+      <ModalContent>
+        <ModalHeader className="flex flex-col gap-1">實體物件篩選</ModalHeader>
+        <ModalBody className="items-center">
+          <div className="absolute inset-0 w-3/4 h-9 top-8 left-4 overflow-x-auto scrollbar-hide">
+            <div className="flex gap-2 items-center justify-left">
+              {categories.map((category, index) => (
+                <Button
+                  key={category + index}
+                  className={`shrink-0 \
+													bg-zinc-50 dark:bg-zinc-800 \
+														text-black dark:text-white \
+														border border-zinc-400 dark:border-zinc-900 \
+														shadow-sm shadow-zinc-300 dark:shadow-zinc-800 
+														${
+                              selectedCategories.includes(category)
+                                ? "bg-textBtnHover dark:bg-textBtnHover text-white dark:text-white"
+                                : ""
+                            }`}
+                  color="primary"
+                  size="sm"
+                  onClick={() => handleCategoryClick(category)}
+                >
+                  {category}
+                </Button>
+              ))}
+            </div>
+          </div>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default FacilityFilter;
