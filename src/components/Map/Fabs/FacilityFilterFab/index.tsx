49b6194dfa3bd06b2b2790fc0c3190bd79bfcf8e
Barney Lin <32840701+linyc0817@users.noreply.github.com>
2024-04-29 15:25:02 +0800

    Add facility filter button and drawer (#130)
    * feat(Marker): :sparkles: Add facility filter button and drawer with options
    
    * feat: :fire: Remove origin facility filter & change display criteria
    
    * feat: :lipstick: Improve facility filter buttons layout
    
    * docs: :memo: Modify shinemood latlng center

--- src/components/Map/Fabs/FacilityFilterFab/index.tsx
+++ src/components/Map/Fabs/FacilityFilterFab/index.tsx
@@ -1,75 +1,45 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Button } from "@nextui-org/react";
-import { getLocationCategories } from "../../../../constants/facility";
 import { useSearchParams } from "react-router-dom";
-import { getParamsFromDrawer } from "../../../../utils/routes/params";
-import { useGetClusterQuery } from "../../../../api/cluster";
-import { useDispatch, useSelector } from "react-redux";
+import { isCurrentDrawerParams } from "../../../../utils/routes/params";
+import { useDispatch } from "react-redux";
 
-import { setSelectedCategories } from "../../../../store/facility";
-import { IRootState } from "../../../../store";
+import facilityMarkerFilter from "../../../../assets/images/facilityMarkerFilter.svg";
+import { openModal } from "../../../../store/modal";
+import FacilityFilter from "../../../modal/FacilityFilter";
 
-const FacilityFilterFabs: React.FC = () => {
+const FacilityFilterFab: React.FC = () => {
   const [searchParams] = useSearchParams();
 
-  const clusterId = getParamsFromDrawer("cluster", searchParams).clusterId;
-  const { data: cluster } = useGetClusterQuery(clusterId);
-
-  const categories = cluster ? getLocationCategories(cluster.data.name) : [];
-
-  const selectedCategories = useSelector(
-    (state: IRootState) => state.facility.selectedCategories,
+  // const clusterId = getParamsFromDrawer("cluster", searchParams).clusterId;
+  const isCurrentSearchParamsCluster = isCurrentDrawerParams(
+    "cluster",
+    searchParams,
   );
 
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    // Initialize selected categories to empty array
-    if (cluster) {
-      dispatch(setSelectedCategories([]));
-    }
-  }, [cluster, dispatch]);
-
-  const handleCategoryClick = (category: string) => {
-    if (selectedCategories.includes(category)) {
-      // If the category is already selected, remove it
-      dispatch(
-        setSelectedCategories(
-          selectedCategories.filter((cat) => cat !== category),
-        ),
-      );
-    } else {
-      // If the category is not selected, add it
-      dispatch(setSelectedCategories([...selectedCategories, category]));
-    }
+  const handleFilterClick = () => {
+    dispatch(openModal("facilityFilter"));
   };
 
-  return (
-    <div className="absolute inset-0 w-3/4 h-9 top-8 left-4 overflow-x-auto scrollbar-hide">
-      <div className="flex gap-2 items-center justify-left">
-        {categories.map((category, index) => (
-          <Button
-            key={category + index}
-            className={`shrink-0 \
-                    bg-zinc-50 dark:bg-zinc-800 \
-                      text-black dark:text-white \
-                      border border-zinc-400 dark:border-zinc-900 \
-                      shadow-sm shadow-zinc-300 dark:shadow-zinc-800 
-                      ${
-                        selectedCategories.includes(category)
-                          ? "bg-textBtnHover dark:bg-textBtnHover text-white dark:text-white"
-                          : ""
-                      }`}
-            color="primary"
-            size="sm"
-            onClick={() => handleCategoryClick(category)}
-          >
-            {category}
-          </Button>
-        ))}
-      </div>
-    </div>
-  );
+  return isCurrentSearchParamsCluster ? (
+    <>
+      <Button
+        key="FacilityFilterFab"
+        className="absolute top-20 right-4 w-10 h-10"
+        isIconOnly
+        size="sm"
+        style={{ backgroundColor: "transparent", padding: 0 }}
+        onClick={() => {
+          handleFilterClick();
+        }}
+      >
+        <img src={facilityMarkerFilter} alt="facilityMarkerFilter" />
+      </Button>
+      <FacilityFilter />
+    </>
+  ) : null;
 };
 
-export default FacilityFilterFabs;
+export default FacilityFilterFab;
