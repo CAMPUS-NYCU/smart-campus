8101b345627ce972a1c4d33b2b3a9ebf8c81e793
Jim <jim901219@gmail.com>
2024-05-17 01:01:46 +0800

    refactor: :fire: Remove clusterId in addReport() in src/store/report/index.ts and also remove the code that use clusterId in AddReportFab

--- src/components/Map/Fabs/AddReportFab/index.tsx
+++ src/components/Map/Fabs/AddReportFab/index.tsx
@@ -2,29 +2,21 @@ import React from "react";
 import { Button } from "@nextui-org/react";
 import { useDispatch } from "react-redux";
 
-import { useSearchParams } from "react-router-dom";
-import { getParamsFromDrawer } from "../../../../utils/routes/params";
 import { useGetUserQuery } from "../../../../api/user";
 import { openModal } from "../../../../store/modal";
 import { addReport } from "../../../../store/report";
 import addReportBtn from "../../../../assets/images/addReport.svg";
 
 const AddReportFab: React.FC = () => {
-  const [searchParams] = useSearchParams();
-
   const dispatch = useDispatch();
-
-  const id = getParamsFromDrawer("cluster", searchParams).clusterId;
 
   const { data: user } = useGetUserQuery();
 
   const handleAddReportClick = () => {
-    if (!id) {
-      throw new Error("ClusterDrawer: id is null");
-    } else if (!user?.id) {
+    if (!user?.id) {
       dispatch(openModal("login"));
     } else {
-      dispatch(addReport({ clusterId: id, createdBy: user?.id }));
+      dispatch(addReport({ createdBy: user?.id }));
     }
   };
 
