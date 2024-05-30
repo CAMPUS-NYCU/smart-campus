4bb99680ea00b4b966d90de9d76a4b85a229f017
linyc <barney0817.cs10@nycu.edu.tw>
2024-05-30 17:07:29 +0800

    feat: :lipstick: Hidden userLocationFab

--- src/components/Map/Fabs/index.tsx
+++ src/components/Map/Fabs/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 import UserFab from "./UserFab";
-import UserLocationFab from "./UserLocationFab";
 import FacilityFilterFabs from "./FacilityFilterFab";
 import LlmFabs from "./LlmFab";
 import AddReportFab from "./AddReportFab";
@@ -11,7 +10,6 @@ const Fabs: React.FC = () => {
   return (
     <>
       <UserFab />
-      <UserLocationFab />
       <FacilityFilterFabs />
       <LlmFabs />
       <AddReportFab />
