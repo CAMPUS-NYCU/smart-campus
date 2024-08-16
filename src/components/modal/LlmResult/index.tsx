102c24d8ccde0bb946ae86e0df6804dd8801205a
linyc <barney0817.cs10@nycu.edu.tw>
2024-08-16 23:13:27 +0800

    fix: :bug: Create getSelectedPois api to get the recommend Pois
    rather using getPoi api with map function, create a new api to solve the not update bug

--- src/components/modal/LlmResult/index.tsx
+++ src/components/modal/LlmResult/index.tsx
@@ -19,8 +19,8 @@ import {
 import { useSearchParams } from "react-router-dom";
 import { useGetUserQuery } from "../../../api/user";
 import { addReport, editReport } from "../../../store/report";
-import { useCallback, useEffect, useState } from "react";
-import { useLazyGetPoiQuery } from "../../../api/poi";
+import { useEffect, useState } from "react";
+import { useGetSelectedPoisQuery } from "../../../api/poi";
 import Poi, { PoiData } from "../../../models/poi";
 import { useTranslation } from "react-i18next";
 import React from "react";
@@ -193,7 +193,6 @@ const LlmResult: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const clusterId = getParamsFromDrawer("cluster", searchParams).clusterId;
   const { data: user } = useGetUserQuery();
-  const [getPoi] = useLazyGetPoiQuery();
 
   const { t } = useTranslation();
 
@@ -205,44 +204,25 @@ const LlmResult: React.FC = () => {
     (state: IRootState) => state.llm.recommendContributions,
   );
 
-  const recommendState = isCurrentDrawerParams("recommend", searchParams);
-  const recommendLoading = useSelector(
-    (state: IRootState) => state.llm.recommendLoading,
-  );
-
   const [recommendPois, setRecommendPois] = useState<Poi[]>([]);
 
-  const fetchData = useCallback(
-    async (recommendContributions: string[]) => {
-      const tasks = recommendContributions.map(async (contribution) => {
-        return getPoi(contribution, true)
-          .unwrap()
-          .then((res) => {
-            if (res === null) throw new Error("No recommend poi found.");
-            else {
-              return res;
-            }
-          });
-      });
-      const res = await Promise.all(tasks);
-      return res;
-    },
-    [getPoi],
-  );
+  const { data: selectedPoiList, isLoading: isPoiListLoading } =
+    useGetSelectedPoisQuery(recommendContributions, {
+      skip: recommendContributions.length === 0,
+    });
 
   useEffect(() => {
-    if (!recommendLoading && recommendState) {
-      fetchData(recommendContributions).then((res) => {
-        setRecommendPois(res);
-      });
+    if (!isPoiListLoading && selectedPoiList && selectedPoiList.length > 0) {
+      setRecommendPois(selectedPoiList);
     }
-  }, [fetchData, recommendContributions, recommendLoading, recommendState]);
+  }, [selectedPoiList, isPoiListLoading]);
 
   const dispatch = useDispatch();
 
   const handleCloseModal = () => {
     // will also clear recommendPois
     dispatch(setRecommendContributions([]));
+    setRecommendPois([]);
     setupDrawerParams<"cluster">({ clusterId }, searchParams, setSearchParams);
     dispatch(closeModal("llmResult"));
   };
